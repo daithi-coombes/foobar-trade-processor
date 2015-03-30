a973deb82bb616755d1a78d1782793bb246173fa
Dáithí Coombes <webeire@gmail.com>
2015-03-30 07:23:35 +0100

    front controller

---
+++ index.php
@@ -0,0 +1,33 @@
+<?php
+/**
+ * Front controller.
+ * 
+ * A market trade processor.
+ * 
+ * @package Foobar
+ * @author Daithi Coombes <webeire@gmail.com>
+ */
+namespace Foobar;
+use Foobar\Router;
+
+//bootstrap
+require_once('bootstrap.php');
+
+//set default module
+if (!@$_POST['module'])
+    $_POST['module'] = 'TradeProcessor';
+
+//route request
+$route = Router::getInstance()
+	->parseGet(null)
+	->parsePost($_POST);    //only parse post requests
+
+
+//controller::action()
+$controller = Controller::factory($route)
+	->doAction();
+
+
+//view()
+$view = View::getView($controller)
+	->render();
